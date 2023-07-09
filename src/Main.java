38ed26897b2c1d00a338dabcb2aaea0ba712437e
VerandaMorina <morinaveranda@gmail.com>
2023-07-09 02:30:53 +0200

    The basics solar system is ready to be deployed

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        solarSystemBasic Omanx =new solarSystemBasic();
+        Omanx.Moon="2";
+        Omanx.Sun="1";
+        Omanx.Planets="2";
+        Omanx.Star="4";
+    }
+}
